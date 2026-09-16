63d0c6b531a901ce0ccb9bfb822abc646c857c04
curiousdave1472 <davidshanlay@gmail.com>
2026-09-16 11:50:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // Write your code below
+//Create the function with key and object
+function addValue(myObj, myValue){
+  //Set the key to myValue
+    myObj['myValue']= myValue;
+}
+//Assign variable for myObj
+var myObj = {};
+
+
+addValue(myObj, true);
+
+console.log(myObj['myValue']);
